c86067e7c319742be757b161dafa0eeca0e2cb4b
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-04-28 19:16:57 +0200

    Milestone 2

--- react-task-manager-front/src/pages/TaskList.jsx
+++ react-task-manager-front/src/pages/TaskList.jsx
@@ -1,4 +1,11 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+
 export default function TaskList() {
+    const { tasks } = useContext(GlobalContext);
+
+    console.log("Tasks:", tasks);
+
     return (
         <h1>Lista delle Task</h1>
     )
